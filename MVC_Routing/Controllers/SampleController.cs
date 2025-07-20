d8b2b7c86541ce3a0a4e0c89f5197209350db751
Bashimane Mamabolo <117764630+Bashimane-Mamabolo@users.noreply.github.com>
2025-07-20 23:33:00 +0200

    Disabled ssl and implemented pages navigation with asp controls

---
+++ MVC_Routing/Controllers/SampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Routing.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
